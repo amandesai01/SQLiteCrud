2eafd4fffd455be8851fa4d3f3030a92d5547f7f
amandesai01 <amandesai01@gmail.com>
2020-01-02 13:30:15 +0530

    Connected View to Activity

--- app/src/main/java/com/example/demo/sqlitecrud/ViewActivity.java
+++ app/src/main/java/com/example/demo/sqlitecrud/ViewActivity.java
@@ -2,12 +2,23 @@ package com.example.demo.sqlitecrud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ViewActivity extends AppCompatActivity {
-
+    TextView tv1;
+    DBHandler db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+        db = MainActivity.db;
+        tv1 = findViewById(R.id.tv1);
+        String data = db.getStudent();
+        if(data.length() == 0){
+            tv1.setText("No Data");
+        }
+        else{
+            tv1.setText(data);
+        }
     }
 }

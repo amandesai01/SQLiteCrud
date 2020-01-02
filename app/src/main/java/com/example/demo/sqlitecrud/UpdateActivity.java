6e0dae9134e5c2bc72aac8e2a99f382b34411c03
amandesai01 <amandesai01@gmail.com>
2020-01-02 13:36:15 +0530

    Connected Update to Activity

--- app/src/main/java/com/example/demo/sqlitecrud/UpdateActivity.java
+++ app/src/main/java/com/example/demo/sqlitecrud/UpdateActivity.java
@@ -2,12 +2,37 @@ package com.example.demo.sqlitecrud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UpdateActivity extends AppCompatActivity {
-
+    EditText etName, etRno;
+    Button btnSave;
+    DBHandler db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+        etName = findViewById(R.id.etName);
+        etRno = findViewById(R.id.etRno);
+        btnSave = findViewById(R.id.btnSave);
+        db = MainActivity.db;
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name  = etName.getText().toString();
+                int rno = Integer.parseInt(etRno.getText().toString());
+                if(name.length() < 2){
+                    etName.setError("Error");
+                    return;
+                }
+                if(rno <= 0){
+                    etRno.setError("Error");
+                    return;
+                }
+                db.updateStudent(rno, name);
+            }
+        });
     }
 }
